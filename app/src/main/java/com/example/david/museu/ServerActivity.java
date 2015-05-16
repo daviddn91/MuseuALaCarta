887dbc6c2e2c7310e1afabd076f363bc7a09c0da
David Delgado <david_dn1991@hotmail.com>
2015-05-16 18:38:08 +0200

    Primer commit

--- app/src/main/java/com/example/david/museu/ServerActivity.java
+++ app/src/main/java/com/example/david/museu/ServerActivity.java
@@ -1,8 +1,10 @@
 package com.example.david.museu;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 
