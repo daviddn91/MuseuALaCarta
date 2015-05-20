33ca58146db6667be0d6284aa18e502895ae8992
David Delgado <david_dn1991@hotmail.com>
2015-05-20 23:29:44 +0200

    Ja puc escriure NFC, ara falta llegir-los

--- app/src/main/java/com/example/david/museu/MainActivity.java
+++ app/src/main/java/com/example/david/museu/MainActivity.java
@@ -15,6 +15,7 @@ import android.nfc.tech.Ndef;
 import android.nfc.tech.NdefFormatable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,8 +23,11 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
+import java.util.Locale;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -92,28 +96,133 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onNewIntent(Intent intent) {
         Toast.makeText(this, "NFC intent recibido!",Toast.LENGTH_LONG).show();
-
         super.onNewIntent(intent);
+
+        if (intent.hasExtra((NfcAdapter.EXTRA_TAG)))
+        {
+            Toast.makeText(this, "NfcIntent!", Toast.LENGTH_SHORT).show();
+
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            NdefMessage ndefMessage = createNdefMessage("My string content!");
+
+            writeNdefMessage(tag, ndefMessage);
+        }
     }
 
     @Override
     protected void onResume() {
-
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-        IntentFilter[] intentFilter= new IntentFilter[]{};
-
-        nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilter, null);
         super.onResume();
+
+        enableForegroundDispatchSystem();
     }
 
     @Override
     protected void onPause(){
-
-        nfcAdapter.disableForegroundDispatch(this);
-
         super.onPause();
+
+        disableForegroundDispatchSystem();
+    }
+
+    private void enableForegroundDispatchSystem() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        IntentFilter[] intentFilters = new IntentFilter[]{};
+
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilters, null);
+    }
+
+    private void disableForegroundDispatchSystem() {
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    private void formatTag(Tag tag, NdefMessage ndefMessage) {
+        try {
+
+            NdefFormatable ndefFormatable = NdefFormatable.get(tag);
+
+            if(ndefFormatable == null)
+            {
+                Toast.makeText(this, "Tag is not ndef formatable!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            ndefFormatable.connect();
+            ndefFormatable.format(ndefMessage);
+            ndefFormatable.close();
+
+            Toast.makeText(this, "Tag writen!", Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            Log.e("formatTag",e.getMessage());
+        }
+    }
+
+    private void writeNdefMessage(Tag tag, NdefMessage ndefMessage)
+    {
+
+        try {
+
+            if(tag == null)
+            {
+                Toast.makeText(this, "Tag object cannot be null", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Ndef ndef = Ndef.get(tag);
+
+            if (ndef == null) {
+                // format tag with the ndef formate and writes the message
+                formatTag(tag, ndefMessage);
+            }else
+            {
+                ndef.connect();
+                if (!ndef.isWritable()) {
+                    Toast.makeText(this, "Tag is not writable", Toast.LENGTH_SHORT).show();
+                    ndef.close();
+                    return;
+                }
+
+                ndef.writeNdefMessage(ndefMessage);
+                ndef.close();
+
+                Toast.makeText(this, "Tag writen!", Toast.LENGTH_SHORT).show();
+            }
+
+        }catch (Exception e) {
+            Log.e("writeNdefMessage",e.getMessage());
+        }
+
+    }
+
+    private NdefRecord createTextRecord(String content) {
+        try {
+            byte[] language;
+            language = Locale.getDefault().getLanguage().getBytes("UTF-8");
+
+            final byte[] text = content.getBytes("UTF-8");
+            final int languageSize = language.length;
+            final int textLength = text.length;
+            final ByteArrayOutputStream payload = new ByteArrayOutputStream(1 + languageSize + textLength);
+
+            payload.write((byte) (languageSize & 0x1F));
+            payload.write(language, 0, languageSize);
+            payload.write(text, 0, textLength);
+
+            return new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, new byte[0], payload.toByteArray());
+
+        } catch(UnsupportedEncodingException e) {
+            Log.e("createTextRecord",e.getMessage());
+        }
+        return null;
+    }
+
+    private NdefMessage createNdefMessage(String content) {
+
+        NdefRecord ndefRecord = createTextRecord(content);
+        NdefMessage ndefMessage = new NdefMessage(new NdefRecord[]{ ndefRecord });
+
+        return ndefMessage;
     }
 }
